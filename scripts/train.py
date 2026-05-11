a3635a341f8f1ef660118a5c69db3099247e2359
Sh-Vacle <150429625+Sh-Vacle@users.noreply.github.com>
2026-05-12 06:41:24 +0800

    remove old project files

--- scripts/train.py
+++
@@ -1,5 +0,0 @@
-from violence_detection.train import main
-
-
-if __name__ == "__main__":
-    main()
--- tests/test_paths.py
+++
@@ -1,9 +0,0 @@
-from violence_detection.paths import safe_stem
-
-
-def test_safe_stem_keeps_normal_name():
-    assert safe_stem("video_01.mp4") == "video_01"
-
-
-def test_safe_stem_replaces_spaces_and_symbols():
-    assert safe_stem("a b#c.mp4") == "a_b_c"
